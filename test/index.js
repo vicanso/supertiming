f32fa19617c8df7d6ef17f09a39f2d480baa5f8f
vicanso <vicansocanbico@gmail.com>
2017-03-02 22:06:39 +0800

    Add `rename` to rename the function name of timing

--- test/index.js
+++ test/index.js
@@ -96,6 +96,35 @@ describe('supertiming', () => {
       .catch(done);
   });
 
+  it('rename the timing name sucessful', (done) => {
+    const timing = new Timing();
+    timing.start('/users/me');
+    timing.start('getUser');
+    timing.start('mongodb:get')
+    delay(30)
+      .then(() => {
+        timing.end('mongodb:get');
+        timing.start('validate:user');
+        return delay(50);
+      })
+      .then(() => {
+        timing.end('validate:user');
+        return delay(10);
+      })
+      .then(() => {
+        timing.rename('getUser', 'getUserNew')
+        timing.end();
+        const data = timing.toJSON(true);
+        assert.equal(data.length, 4);
+        assert.equal(data[0].name, '/users/me');
+        assert.equal(data[1].name, 'getUserNew');
+        assert.equal(data[2].name, 'mongodb:get');
+        assert.equal(data[3].name, 'validate:user');
+        done();
+      })
+      .catch(done);
+  });
+
   it('get server timing sucessful', (done) => {
     const timing = new Timing();
     timing.start('/users/me');
